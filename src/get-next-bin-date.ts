e2fb42bda832ade6a0b4c6e6ab0d6bd3d46e5f45
elliotjharper <50680194+elliotjharper@users.noreply.github.com>
2022-07-09 00:39:15 +0100

    Got a back and forth going

--- src/get-next-bin-date.ts
+++ src/get-next-bin-date.ts
@@ -1,13 +1,17 @@
 import { isSameDay } from 'date-fns';
 import puppeteer from 'puppeteer';
-import { houseNumber, postCode } from './inputs';
+import { houseNumber } from './inputs';
 
 export interface IBinData {
     wasteDate: Date;
     wasteType: string;
 }
 
-async function startBrowser(runningOn: 'pi' | 'mac' | 'win'): Promise<puppeteer.Browser> {
+type BrowserType = 'pi' | 'mac' | 'win' | 'win-headed';
+
+const runningOn: BrowserType = 'win-headed';
+
+async function startBrowser(): Promise<puppeteer.Browser> {
     if (runningOn === 'pi') {
         return await puppeteer.launch({
             headless: true,
@@ -16,12 +20,15 @@ async function startBrowser(runningOn: 'pi' | 'mac' | 'win'): Promise<puppeteer.
             //ignoreDefaultArgs: ['--disable-extensions'],
             // defaultViewport: null,
         });
-    } else if(runningOn === 'mac') {
+    } else if (runningOn === 'mac') {
         return await puppeteer.launch({
             headless: true,
-            executablePath: '/usr/local/bin/chromium'
+            executablePath: '/usr/local/bin/chromium',
         });
-    
+    } else if (runningOn === 'win-headed') {
+        return await puppeteer.launch({
+            headless: false,
+        });
     } else {
         return await puppeteer.launch({
             headless: true,
@@ -38,21 +45,96 @@ async function goToStartPage(page: puppeteer.Page) {
     //console.log('Loading start page...DONE');
 }
 
-async function doPostCodeStep(page: puppeteer.Page) {
+const postCodeSelector = 'input#P153_POST_CODE';
+
+async function tryEnterPostCode(
+    page: puppeteer.Page,
+    postCode: string
+): Promise<boolean> {
+    await page.waitForSelector(postCodeSelector, { visible: true });
+
+    await page.type(postCodeSelector, postCode);
+
+    const postCodeElement = await page.$(postCodeSelector);
+
+    let postCodeElementValue = await page.evaluate(
+        (elem) => elem.value,
+        postCodeElement
+    );
+
+    if (!postCodeElementValue) {
+        console.log('postCodeElement still empty (this means not ready)');
+        return false;
+    }
+
+    if (postCodeElementValue === postCode) {
+        return true;
+    }
+
+    await page.evaluate(
+        (elem, newValue) => {
+            elem.value = newValue;
+        },
+        postCodeElement,
+        postCode
+    );
+
+    postCodeElementValue = await page.evaluate(
+        (elem) => elem.value,
+        postCodeElement
+    );
+
+    return postCodeElementValue === postCode;
+}
+
+async function doPostCodeStep(
+    page: puppeteer.Page,
+    postCode: string
+): Promise<void> {
     //console.log('Enter post code into input...');
-    await page.waitForSelector('input#P153_POST_CODE', { visible: true });
-    await page.type('input#P153_POST_CODE', postCode);
-    //console.log('Enter post code into input...DONE');
+    let counter = 0;
+    while (true) {
+        counter++;
+        console.log(`enter postCode attempt: ${counter}`);
+
+        if (counter > 100) {
+            throw new Error('Too many attempts to enter postcode. Giving up.');
+        }
+
+        if (await tryEnterPostCode(page, postCode)) {
+            break;
+        }
+    }
 
     //console.log('Submit post code...');
     await page.waitForSelector('button[title="Search"]');
     await page.click('button[title="Search"]');
-    //console.log('Submit post code...DONE');
+    console.log('Submit post code...DONE');
 
     await page.waitForNavigation();
+
+    console.log('post code...navigation done!');
 }
 
-async function doStreetAddressStep(page: puppeteer.Page) {
+async function readStreetAddresses(page: puppeteer.Page): Promise<string[]> {
+    await page.waitForXPath('//*[@id="P153_UPRN"]/option[2]');
+
+    const targetOptions = await page.$x(`//*[@id="P153_UPRN"]/option`);
+
+    const streetAddresses: string[] = [];
+
+    for (let i = 0; i < targetOptions.length; i++) {
+        const targetOption = targetOptions[i];
+        const streetAddress = await targetOption.evaluate((elem) => {
+            return (elem as HTMLOptionElement).text;
+        });
+        streetAddresses.push(streetAddress);
+    }
+
+    return streetAddresses;
+}
+
+async function doStreetAddressStep(page: puppeteer.Page): Promise<void> {
     //console.log('Select house number...');
     const [targetOption] = await page.$x(
         `//*[@id="P153_UPRN"]/option[contains(text(), "${houseNumber}")]`
@@ -72,7 +154,9 @@ async function doStreetAddressStep(page: puppeteer.Page) {
     await page.waitForNetworkIdle();
 }
 
-async function collectBinDataFromPage(page: puppeteer.Page): Promise<IBinData[]> {
+async function collectBinDataFromPage(
+    page: puppeteer.Page
+): Promise<IBinData[]> {
     // get the two tables: ul#CollectionDay_report li
     const wasteListItems = await page.$$('ul#CollectionDay_report li');
     let wasteData: IBinData[] = [];
@@ -86,7 +170,9 @@ async function collectBinDataFromPage(page: puppeteer.Page): Promise<IBinData[]>
         );
 
         // get the date
-        const [dateElement] = await wasteListItem.$x('.//p[contains(@style, "padding-left:55px")]');
+        const [dateElement] = await wasteListItem.$x(
+            './/p[contains(@style, "padding-left:55px")]'
+        );
         const wasteDateText = await dateElement.evaluate(
             (actualDateElement) => actualDateElement.innerHTML
         );
@@ -102,15 +188,42 @@ async function collectBinDataFromPage(page: puppeteer.Page): Promise<IBinData[]>
     return wasteData;
 }
 
-export async function getNextBinDate(): Promise<IBinData> {
+export async function getStreetAddresses(postCode: string): Promise<string[]> {
     // https://github.com/puppeteer/puppeteer/issues/2924#issuecomment-880992772
 
-    const browser: puppeteer.Browser = await startBrowser('mac');
+    console.log('Starting Browser...');
+    const browser: puppeteer.Browser = await startBrowser();
+    console.log('Started Browser');
+
     const page: puppeteer.Page = await browser.newPage();
 
     await goToStartPage(page);
 
-    await doPostCodeStep(page);
+    console.log(`Entering post code: ${postCode}`);
+    await doPostCodeStep(page, postCode);
+    console.log('Post code submitted');
+
+    console.log('Getting street addresses');
+    const streetAddresses = await readStreetAddresses(page);
+    console.log(`Got street addresses:`);
+    for (const streetAddress of streetAddresses) {
+        console.log(streetAddress);
+    }
+
+    await browser.close();
+
+    return streetAddresses;
+}
+
+export async function getNextBinDate(): Promise<IBinData> {
+    // https://github.com/puppeteer/puppeteer/issues/2924#issuecomment-880992772
+
+    const browser: puppeteer.Browser = await startBrowser();
+    const page: puppeteer.Page = await browser.newPage();
+
+    await goToStartPage(page);
+
+    await doPostCodeStep(page, 'aa11 1aa');
 
     await doStreetAddressStep(page);
 
@@ -138,7 +251,9 @@ type BinDataCacheValue = [Date, Promise<IBinData>];
 type BinDataCache = BinDataCacheValue | undefined;
 let staticBinDataCache: BinDataCache = undefined;
 
-function internalIsCacheReady(binDataCache: BinDataCache): binDataCache is BinDataCacheValue {
+function internalIsCacheReady(
+    binDataCache: BinDataCache
+): binDataCache is BinDataCacheValue {
     return !!binDataCache && isSameDay(binDataCache[0], new Date());
 }
 
--- src/talkback-bot.ts
+++ src/talkback-bot.ts
@@ -1,17 +1,41 @@
-import { Bot } from 'grammy';
 import { readFileSync } from 'fs';
-import { getCachedBinData, getNextBinDate } from './get-next-bin-date';
+import { Bot, Context, InlineKeyboard, session, SessionFlavor } from 'grammy';
+import { getCachedBinData, getStreetAddresses } from './get-next-bin-date';
+
+type BinBotState =
+    | 'awaitingPostCodeInput'
+    | 'awaitingHouseNumberInput'
+    | 'awaitingInputsConfirmation'
+    | 'gettingStreetAddresses'
+    | 'awaitingStreetAddressConfirmation';
+
+interface IBinSession {
+    state?: BinBotState;
+
+    postCode?: string;
+    houseNumber?: string;
+    streetAddresses?: string[];
+    streetAddress?: string;
+}
+
+type BinBotContext = Context & SessionFlavor<IBinSession>;
 
 const botToken = readFileSync('./token.txt', { encoding: 'utf-8' });
 
-if(!botToken) {
+if (!botToken) {
     throw new Error('No token provided!');
 }
 
-const telegramBot = new Bot(botToken);
+const binBot = new Bot<BinBotContext>(botToken);
 
-telegramBot.command('bins', async (context) => {
-    console.log('on:/bins');
+function createInitialBinSession(): IBinSession {
+    return {};
+}
+
+binBot.use(session({ initial: createInitialBinSession }));
+
+binBot.command('bins_hardcoded', async (context) => {
+    console.log('on:/bins-hardcoded');
 
     await context.reply('Getting bins data!');
 
@@ -23,16 +47,134 @@ telegramBot.command('bins', async (context) => {
     console.log('bin data sent back!');
 });
 
-telegramBot.on('message', async (context) => {
+binBot.command('bins_advanced', async (context) => {
+    console.log('on:/bins-advanced');
+    context.session.state = 'awaitingPostCodeInput';
+    await context.reply('What is your post code?');
+});
+
+async function abortByMessage(context: BinBotContext): Promise<void> {
+    context.session = createInitialBinSession();
+    await context.reply('Cancellation received. Going back to standby...');
+}
+
+async function abortByCallbackQuery(context: BinBotContext): Promise<void> {
+    context.session = createInitialBinSession();
+    await context.answerCallbackQuery();
+    await context.reply('Cancellation received. Going back to standby...');
+}
+
+async function sendBusyStatus(context: BinBotContext): Promise<void> {
+    await context.reply(
+        'Currently awaiting response to a prompt. Until response is received messages are ignored.'
+    );
+}
+
+const confirmInputsQuery = 'confirm-inputs';
+const abortQuery = 'abort';
+
+async function handlePostcodeResponse(context: BinBotContext): Promise<void> {
+    context.session.state = 'awaitingHouseNumberInput';
+    context.session.postCode = context.message?.text;
+    await context.reply('What is your house number?');
+}
+
+async function handleHouseNumberResponse(
+    context: BinBotContext
+): Promise<void> {
+    context.session.state = 'awaitingInputsConfirmation';
+    context.session.houseNumber = context.message?.text;
+
+    const inlineKeyboard = new InlineKeyboard();
+    inlineKeyboard.text('Yes', confirmInputsQuery);
+    inlineKeyboard.text('No', abortQuery);
+
+    await context.reply(
+        `Are these correct? Post code: ${context.session.postCode} House number: ${context.session.houseNumber}`,
+        { reply_markup: inlineKeyboard }
+    );
+}
+
+binBot.on('message', async (context) => {
     console.log('on:message...');
+
+    if (context.session.state === 'awaitingPostCodeInput') {
+        return await handlePostcodeResponse(context);
+    }
+
+    if (context.session.state === 'awaitingHouseNumberInput') {
+        return await handleHouseNumberResponse(context);
+    }
+
+    if (context.session.state === 'awaitingInputsConfirmation') {
+        return await sendBusyStatus(context);
+    }
 
     await context.reply(`Talking back! You said: ${context.message.text}`);
 
     console.log('message sent back!');
 });
 
+binBot.callbackQuery(confirmInputsQuery, async (context) => {
+    context.session.state = 'gettingStreetAddresses';
+    await context.answerCallbackQuery();
+    await context.reply(
+        'Thanks for confirming your inputs. Gathering street addresses now...'
+    );
+
+    if (!context.session.postCode || !context.session.houseNumber) {
+        throw new Error(
+            'Was about to get street addresses but did not have postCode'
+        );
+    }
+
+    const streetAddresses = await getStreetAddresses(context.session.postCode);
+
+    // await context.reply('Got addresses:');
+    // for (const streetAddress of streetAddresses) {
+    //     await context.reply(streetAddress);
+    // }
+
+    if (!streetAddresses?.length) {
+        await context.reply(
+            'Did not get any street addresses for the post code you provided.'
+        );
+        return;
+    }
+
+    const usersStreetAddress = streetAddresses.find((streetAddress) => {
+        return streetAddress.startsWith(
+            context.session.houseNumber!.toString()
+        );
+    });
+
+    if (!usersStreetAddress) {
+        await context.reply(
+            'Could not find your house number in the list of street addresses:'
+        );
+
+        for (const streetAddress of streetAddresses) {
+            await context.reply(streetAddress);
+        }
+
+        return;
+    }
+
+    context.session.streetAddress = usersStreetAddress;
+    context.session.state = 'awaitingStreetAddressConfirmation';
+    await context.reply(`Is this your address? ${usersStreetAddress}`);
+});
+
+binBot.callbackQuery(abortQuery, async (context) => {
+    await abortByCallbackQuery(context);
+});
+
+// binBot.on('callback_query:data', async (context) => {
+
+// });
+
 console.log('starting bot...');
 
-telegramBot.start();
+binBot.start();
 
 console.log('bot started!');
